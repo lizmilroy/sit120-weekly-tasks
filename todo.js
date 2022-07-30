2697d6463c4bfa671fb383eaab5d651a94fdff6d
lizmilroy <elizmilroy@gmail.com>
2022-07-30 19:07:46 +1000

    week 1 complete

---
+++ todo.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: '#vue-application',
+    data: {
+        todos: [
+            { title: 'Yes', checked: false },
+            { title: 'No', checked: false},
+            { title: 'Maybe?', checked: false},
+        ],
+    },
+});
